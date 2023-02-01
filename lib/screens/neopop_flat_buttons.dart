b8079534d4007f3269748fc749d6a77a97a4611a
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-02-01 10:23:04 +0500

    Initial Commit

---
+++ lib/screens/neopop_flat_buttons.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:neopop/neopop.dart';
+import 'package:neopop/utils/color_utils.dart';
+
+import '../utils/constants.dart';
+
+class NeoPopFlatButtons extends StatefulWidget {
+  const NeoPopFlatButtons({super.key});
+
+  @override
+  State<NeoPopFlatButtons> createState() => _NeoPopFlatButtonsState();
+}
+
+class _NeoPopFlatButtonsState extends State<NeoPopFlatButtons> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('NeoPOP Flat Buttons'),
+          centerTitle: true,
+        ),
+        body: Container(
+          color: Colors.black,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: kSmallHeight),
+                NeoPopButton(
+                  color: Colors.white,
+                  onTapUp: () => HapticFeedback.vibrate(),
+                  onTapDown: () => HapticFeedback.vibrate(),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Elevated NeoPOP Button"),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: kLargeHeight),
+                NeoPopButton(
+                  color: Colors.white,
+                  onTapUp: () => HapticFeedback.vibrate(),
+                  onTapDown: () => HapticFeedback.vibrate(),
+                  parentColor: Colors.transparent,
+                  buttonPosition: Position.center,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text("Flat NeoPOP Button"),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: kLargeHeight),
+                const NeoPopShimmer(
+                  shimmerColor: Colors.yellow,
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 100, vertical: 15),
+                    child: Text(
+                      "Shimmer NeoPOP Button",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: kLargeHeight),
+                NeoPopButton(
+                  color: const Color.fromRGBO(0, 0, 0, 1),
+                  buttonPosition: Position.center,
+                  onTapUp: () {},
+                  border: const Border.fromBorderSide(
+                    BorderSide(
+                        color: kBorderColorWhite, width: kButtonBorderWidth),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text("Flat Strokes NeoPOP Button",
+                            style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: kLargeHeight),
+                NeoPopButton(
+                  color: kSecondaryButtonLightColor,
+                  bottomShadowColor:
+                      ColorUtils.getVerticalShadow(kBorderColorGreen).toColor(),
+                  rightShadowColor:
+                      ColorUtils.getHorizontalShadow(kBorderColorGreen)
+                          .toColor(),
+                  animationDuration: kButtonAnimationDuration,
+                  depth: kButtonDepth,
+                  onTapUp: () {},
+                  border: Border.all(
+                    color: kBorderColorGreen,
+                    width: kButtonBorderWidth,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text("Elevated Strokes NeoPOP Button",
+                            style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: kLargeHeight),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    NeoPopButton(
+                      color: kSecondaryButtonLightColor,
+                      bottomShadowColor: kShadowColorDarkGreen,
+                      rightShadowColor: kShadowColorGreen,
+                      buttonPosition: Position.fullBottom,
+                      onTapUp: () {},
+                      border: Border.all(
+                        color: kBorderColorGreen,
+                        width: kButtonBorderWidth,
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 15.0),
+                        child: Text(
+                          "Adjacent Vertically Aligned",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    NeoPopButton(
+                      color: kPrimaryButtonColor,
+                      buttonPosition: Position.fullBottom,
+                      onTapUp: () {},
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 15.0),
+                        child: Text("NeoPOP Buttons"),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: kLargeHeight),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
+                      child: NeoPopButton(
+                        color: kPrimaryButtonColor,
+                        buttonPosition: Position.fullBottom,
+                        depth: kButtonDepth,
+                        onTapUp: () {},
+                        child: const Center(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8.0, vertical: 15.0),
+                            child: Text("Adjacent Horizontally Aligned"),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: NeoPopButton(
+                        color: kSecondaryButtonLightColor,
+                        bottomShadowColor: kShadowColorDarkGreen,
+                        rightShadowColor: kShadowColorGreen,
+                        buttonPosition: Position.fullBottom,
+                        depth: kButtonDepth,
+                        onTapUp: () {},
+                        border: Border.all(
+                          color: kBorderColorGreen,
+                          width: kButtonBorderWidth,
+                        ),
+                        child: const Center(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8.0, vertical: 15.0),
+                            child: Text(
+                              "NeoPOP Buttons",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: kLargeHeight),
+              ],
+            ),
+          ),
+        ),
+      );
+}
---
+++ lib/screens/neopop_titlted_buttons.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:neopop/neopop.dart';
+
+import '/utils/constants.dart';
+
+class NeoPopTiltedButtons extends StatelessWidget {
+  const NeoPopTiltedButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) =>
+    Scaffold(
+      appBar: AppBar(
+        title: const Text('NeoPOP Tilted Buttons'),
+        centerTitle: true,
+      ),
+      body: Container(
+        // height: MediaQuery.of(context).size.height * 1.0,
+        color: Colors.black,
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: kSmallHeight),
+              NeoPopTiltedButton(
+                color: kPrimaryButtonColor,
+                onTapUp: () {},
+                child: const Padding(
+                  padding:  EdgeInsets.symmetric(
+                    horizontal: 80.0,
+                    vertical: 15,
+                  ),
+                  child: Text('Non Floating NeoPopTiltedButton'),
+                ),
+              ),
+              const SizedBox(height: kLargeHeight),
+              NeoPopTiltedButton(
+                isFloating: true,
+                onTapUp: () => HapticFeedback.vibrate(),
+                onTapDown: () => HapticFeedback.vibrate(),
+                decoration: const NeoPopTiltedButtonDecoration(
+                  color: kPopYellowColor,
+                  plunkColor: kPlunkColorYellow,
+                  shadowColor: kTiltedButtonShadowColor,
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 70.0,
+                    vertical: 15,
+                  ),
+                  child: Text('Floating NeoPopTiltedButton'),
+                ),
+              ),
+              const SizedBox(height: kLargeHeight),
+              NeoPopTiltedButton(
+                isFloating: true,
+                onTapUp: () {},
+                decoration: const NeoPopTiltedButtonDecoration(
+                  color: Color(0xFF0D0D0D),
+                  plunkColor: Color(0xFF3F6915),
+                  shadowColor: Colors.black,
+                  border: Border.fromBorderSide(
+                    BorderSide(color: Color(0xFF8DD04A), width: 1),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 70.0, vertical: 15),
+                  child: Text('Strokes NeoPopTiltedButton', style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              const SizedBox(height: kLargeHeight),
+              NeoPopTiltedButton(
+                isFloating: true,
+                onTapUp: () {},
+                decoration: const NeoPopTiltedButtonDecoration(
+                  color: Color.fromRGBO(255, 235, 52, 1),
+                  plunkColor: Color.fromRGBO(255, 235, 52, 1),
+                  shadowColor: Color.fromRGBO(36, 36, 36, 1),
+                  showShimmer: true,
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 70.0,
+                    vertical: 15,
+                  ),
+                  child: Text('Shimmer NeoPopTiltedButton'),
+                ),
+              ),
+            ],
+          ),
+        )
+      ),
+    );
+  }
